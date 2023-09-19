dc95a45b321c72b1fa7eb457220d0180edb8422d
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-19 16:01:15 +0300

    uploadthing setup complated

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { X } from "lucide-react"
+import Image from "next/image"
+
 import { UploadDropzone } from "@/lib/uploadthing"
 
 import "@uploadthing/react/styles.css"
@@ -11,11 +14,26 @@ interface FileUploadProps {
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop()
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute right-0 top-0 shadow-sm"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-        onChange(res?.[0].fileUrl)
+        onChange(res?.[0].url)
       }}
       onUploadError={(error: Error) => {
         console.log(error)
