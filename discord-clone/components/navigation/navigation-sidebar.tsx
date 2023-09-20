895f9979bb4334d7ea33c28cd42928af6bd2913b
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-20 15:55:54 +0300

    complated navigation item component

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,9 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirect } from "next/navigation"
 import { NavigationAction } from "./navigation-action"
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { NavigationItem } from "./navigation-item"
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile()
@@ -20,6 +23,18 @@ const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   )
 }
