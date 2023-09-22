8d9c100eb9f54e3dc39939654cb3be9379fdbf88
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-22 13:26:37 +0300

    created: leave server component

--- discord-clone/components/server/server-header.tsx
+++ discord-clone/components/server/server-header.tsx
@@ -89,7 +89,12 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+            onClick={() => {
+              onOpen("leaveServer")
+            }}
+          >
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
--- discord-clone/hooks/use-modal-store.ts
+++ discord-clone/hooks/use-modal-store.ts
@@ -6,6 +6,7 @@ export type ModalType =
   | "editServer"
   | "members"
   | "createChannel"
+  | "leaveServer"
 
 interface ModalData {
   server?: Server
