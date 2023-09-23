d6f176792d2d1c2a1b70fb38723629b99c1dee4d
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-23 14:33:46 +0300

    server section compnet created

--- discord-clone/components/server/server-sidebar.tsx
+++ discord-clone/components/server/server-sidebar.tsx
@@ -6,6 +6,8 @@ import ServerHeader from "./server-header"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { ServerSearch } from "./server-search"
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react"
+import { Separator } from "@/components/ui/separator"
+import { ServerSection } from "./server-section"
 
 interface ServerSidebarProps {
   serverId: string
@@ -116,6 +118,20 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            {textChannels.map((channel) => (
+              <ServerChannel />
+            ))}
+          </div>
+        )}
       </ScrollArea>
     </div>
   )
