8a05c3deac68eef233be41047946a19f6fab8f68
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-24 16:37:56 +0300

    conversation setup complated

--- discord-clone/app/(auth)/layout.tsx
+++ discord-clone/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex items-center justify-center">{children}</div>
+  return (
+    <div className="flex w-full items-center justify-center h-full bg-[url('https://picsum.photos/1920/1080.webp/?blur')] bg-cover bg-no-repeat bg-center">
+      {children}
+    </div>
+  )
 }
 
 export default AuthLayout
