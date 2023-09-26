029a494fa952e4f7df5bbb5236cf7b80ee3eb984
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-26 14:58:41 +0300

    chat socket hook complated

--- discord-clone/components/chat/chat-messages.tsx
+++ discord-clone/components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react"
 import { Fragment } from "react"
 import { ChatItem } from "./chat-item"
 import { format } from "date-fns"
+import { useChatSocket } from "@/hooks/use-chat-socket"
 
 const DATE_FORMAT = "d MMM yyyy HH:mm"
 
@@ -39,6 +40,8 @@ export const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+  const addKey = `chat:${chatId}:messages`
+  const updateKey = `chat:${chatId}:messages:update`
   const queryKey = `chat:${chatId}`
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
@@ -47,6 +50,7 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     })
+  useChatSocket({ queryKey, addKey, updateKey })
 
   if (status === "loading") {
     return (
