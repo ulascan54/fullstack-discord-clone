b32c6cd1047537f95f271256372a1717c958437f
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-27 18:34:49 +0300

    typo fixed

--- discord-clone/components/server/server-member.tsx
+++ discord-clone/components/server/server-member.tsx
@@ -33,7 +33,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/70 transition mb-1",
-        params.memberId === member.id && "bg-zinc-200/20 dark:bg-zinc-700"
+        params?.memberId === member.id && "bg-zinc-200/20 dark:bg-zinc-700"
       )}
     >
       <UserAvatar
